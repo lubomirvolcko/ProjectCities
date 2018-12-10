5163bb94c77f31f87acf1b2be3ce30da11643204
lubomirvolcko <lubomir.volcko@akademiasovy.sk>
2018-12-10 22:30:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Cities from './Cities/Cities'
+
+class App extends Component {
+
+  state = {
+    cities:[{city:'Kosice', country:'SK'},
+    {city:'Amsterdam',country:'NL'},
+    {city:'London',country:'UK'},
+    {city:'Madrid',country:'ES'},
+    {city:'Detroit',country:'US'}]
+  };
+
+
+
+  
+
+
+
+
+
+  render() {
+    return (
+      <Cities list={this.state.cities}/>
+    );
+  }
+}
+
+export default App;
